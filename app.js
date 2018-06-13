5fc56828c778c344728a4fada1a30366fd531a3e
j.majurel <jeanfelix.majurel@gmail.com>
2018-06-13 18:49:58 +0100

    improve dataset

--- app.js
+++ app.js
@@ -8,28 +8,72 @@ d3.select('svg')
 
 const dateExtracter = /^(\d{4})(\d{2})(\d{2})$/g ;
 
-const playerFormatter = (string) => d3.csvParseRows(string, function(d) {
+const playerFormatter = (text) => {
 
-  let [_, year, month, day] = dateExtracter.exec(d[4]);
+  return d3.csvParseRows(text, row => {
 
+    let result = dateExtracter.exec(row[4]);
+    let date = result ? `${result[1]}-${result[2]}-${result[3]}`: null;
+
+    return {
+      id: +row[0],
+      firstName: row[1],
+      lastName: row[2],
+      hand: row[3],
+      countryCode: row[5],
+      birthDate: date
+    };
+  });
+};
+
+const rankFormatter = (text) => {
+
+  return d3.csvParseRows(text, row => {
+
+    let result = dateExtracter.exec(row[0]);
+    let date = result ? `${result[1]}-${result[2]}-${result[3]}`: null;
+
+    return {
+      rankingDate: date,
+      ranking: +row[1],
+      playerId: +row[2],
+      rankingPoint: +row[3]
+    };
+  })
+};
+
+function tsvFormatter(data, _, header) {
   return {
-    id: d[0],
-    firstName: d[1],
-    lastName: d[2],
-    hand: d[3],
-    birthDate: `${year}-${month}-${day}`,
-    countryCode: d[5]
-  }
-});
-
+    id: +data[header[0]],
+    name: data[header[1]]
+  };
+};
 
 d3.queue()
   .defer(d3.json, 'https://unpkg.com/world-atlas@1/world/110m.json')
-  .defer(d3.csv, 'https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_players.csv', playerFormatter)
-  .defer(d3.csv, 'https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_rankings_current.csv')
-  .await((err, topoJson, players, rankings) => {
+  .defer(d3.tsv, 'https://raw.githubusercontent.com/KoGor/Maps.GeoInfo/master/world-country-names.tsv', tsvFormatter)
+  .defer(d3.json, 'https://github.com/mledoze/countries/blob/442472de98e80f4a44f1028960dbb0dfb1d942fe/dist/countries.json')
+  .defer(d3.text, 'https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_players.csv')
+  .defer(d3.text, 'https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_rankings_current.csv')
+  .await((err, topoJson, countryNames, countries, rawPlayers, rawRankings) => {
+
     if(err) throw err;
+    console.log(countryNames);
+
+
+    /* Format raw data */
+    const players = playerFormatter(rawPlayers); 
+    const rankings = playerFormatter(rawRankings);
+
     const data = topojson.feature(topoJson, topoJson.objects.countries).features;
+
+    //Add country name to map
+    data.forEach(d => {
+      let foundCountry = countryNames.find(country => country.id === +d.id);
+      d.properties.players = players.filter(pl => pl.countryCode  === foundCountry.name);
+      d.properties.name = foundCountry ? foundCountry.name : undefined;
+    });
+    console.log(data);
 
     const projection = d3.geoMercator()
                          .translate([width/2 -10, height/2 +80])
@@ -51,12 +95,14 @@ d3.queue()
 
             d3.select(d3.event.target)
               .transition()
+		.delay(100)
               .style('fill', 'blue');
 
           })
           .on('mouseout', () => {
             d3.select(d3.event.target)
               .transition()
+		.delay(100)
               .style('fill', 'grey')
           })
 
