772d3290932d3ad08883fe5c266583f61abba68c
j.majurel <jeanfelix.majurel@gmail.com>
2018-06-15 19:15:01 +0100

    build map with right projection

--- app.js
+++ app.js
@@ -53,10 +53,10 @@ d3.queue()
 /*  .defer(d3.json, 'https://unpkg.com/world-atlas@1/world/110m.json')
   .defer(d3.tsv, 'https://raw.githubusercontent.com/KoGor/Maps.GeoInfo/master/world-country-names.tsv', tsvFormatter)
   .defer(d3.json, 'https://github.com/mledoze/countries/blob/442472de98e80f4a44f1028960dbb0dfb1d942fe/dist/countries.json')*/
-  .defer(d3.json, './data/easy-countries-topo.json')
+  .defer(d3.json, './data/finalmap.json')
   .defer(d3.text, 'https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_players.csv')
   .defer(d3.text, 'https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_rankings_current.csv')
-  .await((err, topoJson, rawPlayers, rawRankings) => {
+  .await((err, world, rawPlayers, rawRankings) => {
 
     if(err) throw err;
 
@@ -64,7 +64,7 @@ d3.queue()
     const players = playerFormatter(rawPlayers); 
     const rankings = playerFormatter(rawRankings);
 
-    const data = topojson.feature(topoJson, topoJson.objects.countryMap).features;
+    //const data = topojson.feature(topoJson, topoJson.objects.countryMap).features;
 
     //Add country name to map
 /*    data.forEach(d => {
@@ -72,16 +72,15 @@ d3.queue()
       d.properties.players = players.filter(pl => pl.countryCode  === foundCountry.name);
       d.properties.name = foundCountry ? foundCountry.name : undefined;
     });*/
+    console.log(world.features);
 
-    const projection = d3.geoMercator()
-                         .clipExtent([[0, 10], [width, height]])
+    world(country => country.properties.players = rawPlayers.filter(pl => pl.countryCode === country.code))
 
-
-    const path = d3.geoPath(projection);
+    const path = d3.geoPath();
 
     const updateMap = d3.select('svg')
                         .selectAll('path')
-                          .data(data);
+                          .data(world.features);
 
     updateMap
 	.enter()
@@ -89,6 +88,7 @@ d3.queue()
 	  .attr('d', path)
           .style('fill', 'grey')
           .on('mouseover touchover', (d, i, nodes) => {
+	    console.log(d.properties.players);
 
             d3.select(d3.event.target)
               .transition()
